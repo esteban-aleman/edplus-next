955473527b50e3f82fd9314427b61794e0c9e874
Esteban Aleman <ealeman@konradgroup.com>
2022-07-01 17:17:53 -0600

    test: simple playwright test

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test';
 
-test('should navigate to the home page', async ({ page }) => {
+test('should navigate to Home and alternative language', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveURL('http://localhost:3000/');
+  await expect(page).toHaveURL('http://localhost:3000');
+  await page.locator(`text=English`).click();
+  await expect(page).toHaveURL('http://localhost:3000/en');
 });
